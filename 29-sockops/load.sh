9dfd5babaa452e9712d5cffe5f4a203f037fbacf
yunwei37 <1067852565@qq.com>
2023-12-06 10:06:52 +0000

    Deploying to gh-pages from @ eunomia-bpf/bpf-developer-tutorial@6a008e9efe8fba2b7709b8464d55446cd4dbddd9 🚀

--- 29-sockops/load.sh
+++ 29-sockops/load.sh
@@ -2,19 +2,19 @@
 set -x
 set -e
 
-# Mount bpf filesystem
 sudo mount -t bpf bpf /sys/fs/bpf/
 
-# Load the bpf_sockops program
-sudo bpftool prog load bpf_sockops.o "/sys/fs/bpf/bpf_sockop"
-sudo bpftool cgroup attach "/sys/fs/cgroup/unified/" sock_ops pinned "/sys/fs/bpf/bpf_sockop"
-
-MAP_ID=$(sudo bpftool prog show pinned "/sys/fs/bpf/bpf_sockop" | grep -o -E 'map_ids [0-9]+' | awk '{print $2}')
-sudo bpftool map pin id $MAP_ID "/sys/fs/bpf/sock_ops_map"
-
-# Load the bpf_redir program
-if [ -z $1 ]
-then
-   sudo bpftool prog load bpf_redir.o "/sys/fs/bpf/bpf_redir" map name sock_ops_map pinned "/sys/fs/bpf/sock_ops_map"
-   sudo bpftool prog attach pinned "/sys/fs/bpf/bpf_redir" msg_verdict pinned "/sys/fs/bpf/sock_ops_map"
+# check if old program already loaded
+if [ -e "/sys/fs/bpf/bpf_sockops" ]; then
+    echo ">>> bpf_sockops already loaded, uninstalling..."
+    ./unload.sh
+    echo ">>> old program already deleted..."
 fi
+
+# load and attach sock_ops program
+sudo bpftool prog load bpf_contrack.bpf.o /sys/fs/bpf/bpf_sockops type sockops pinmaps /sys/fs/bpf/
+sudo bpftool cgroup attach "/sys/fs/cgroup/" sock_ops pinned "/sys/fs/bpf/bpf_sockops"
+
+# load and attach sk_msg program 
+sudo bpftool prog load bpf_redirect.bpf.o "/sys/fs/bpf/bpf_redir" map name sock_ops_map pinned "/sys/fs/bpf/sock_ops_map"
+sudo bpftool prog attach pinned /sys/fs/bpf/bpf_redir msg_verdict pinned /sys/fs/bpf/sock_ops_map
---
+++ 29-sockops/trace_lo_traffic.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# 5001 is the default port for iperf server
+tcpdump -i lo port 5001
