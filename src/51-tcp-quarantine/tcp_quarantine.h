e489a28c0897ee48ea5a0b200448eee8c08f9e03
LinuxDev9002 <i@littlefisher.me>
2026-07-18 01:42:39 -0700

    Add targeted TCP connection quarantine tool

---
+++ src/51-tcp-quarantine/tcp_quarantine.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: (LGPL-2.1 OR BSD-2-Clause) */
+#ifndef __TCP_QUARANTINE_H
+#define __TCP_QUARANTINE_H
+
+struct quarantine_stats {
+	unsigned long long scanned;
+	unsigned long long established;
+	unsigned long long matched;
+	unsigned long long destroyed;
+	unsigned long long failed;
+};
+
+#endif /* __TCP_QUARANTINE_H */
